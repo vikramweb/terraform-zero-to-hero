4ecbb59fc89fff8226d56a80247a51049843dd4d
Vicky <52860487+vikramweb@users.noreply.github.com>
2024-03-09 20:13:53 +0530

    Update main_1.tf
    comments added

--- Day-8/main_1.tf
+++ Day-8/main_1.tf
@@ -1,9 +1,10 @@
+# MIGRATION TO TERRAFORM
 provider "aws" {
  region = "us-east-1"
 }
 
 import {
-  id = "<instanceID>"
+  id = "<instanceID>" # Change with ur existing instance ID
 
   to = aws.instance.example
 }
